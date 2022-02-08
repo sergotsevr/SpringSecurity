e9ebf44a2a397a66e863cae31841d7fc38d8c145
sgotsev <sgotsev@cinimex.ru>
2022-02-08 16:39:39 +0300

    basic InMemory

--- src/main/java/security/config/SecurityConfiguration.java
+++ src/main/java/security/config/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +20,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         super.configure(http);
     }
 
+    @Bean
     @Override
     protected UserDetailsService userDetailsService(){
-        return super.userDetailsService();
+        return  new InMemoryUserDetailsManager(User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("ADMIN")
+                .build()
+        );
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(8);
     }
 }
